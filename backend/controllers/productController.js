2300e7fadf3fd0fe164a95c2c11ba59d7ffc7a06
Arohan Harsh Dubey <98662859+Arohan24@users.noreply.github.com>
2024-02-06 16:19:47 +0000

    Create Product module added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,15 @@
+const Product=require("../models/productModels")
+
+//Create Product
+exports.createProduct=async (req,res,next) => {
+  const product=await Product.createProduct(req.body);
+
+  res.status(201).json({
+    sucess:true,
+    product
+  })
+}
+
 exports.getAllProducts=(req,res) => {
   res.status(200).json({message:"Route is working Fine"})
 }
