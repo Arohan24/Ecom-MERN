1964b3b94b0bdf33acc325637af708f57cbc84d9
Arohan Harsh Dubey <98662859+Arohan24@users.noreply.github.com>
2024-02-06 13:32:29 +0000

    Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app= require("./app");
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/dbConnect");
 
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({path:"backend/config/config.env"});
+//Connecting to DB
+connectDatabase();
 
 app.listen(process.env.PORT,() => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
