fc6efeb6ed72b4e1c2760b8a2f7e3850d70d862a
Arohan24 <coool.aro24@gmail.com>
2024-02-08 23:42:11 +0530

    Get all Product Route and Update Product routes added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
       success: true,
-      product
+      product,
     });
   } catch (error) {
     next(error); // Pass error to the error handling middleware
@@ -19,9 +19,29 @@ exports.getAllProducts = async (req, res, next) => {
     const products = await Product.find();
     res.status(200).json({
       success: true,
-      products
+      products,
     });
   } catch (error) {
     next(error); // Pass error to the error handling middleware
   }
+};
+
+exports.updateProduct = async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      messsage: "product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidator: true,
+    useFindAndModify: false,
+  });
+
+  req.status(200).json({
+    success:true,
+    product
+  })
 };
