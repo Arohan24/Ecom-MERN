5e263b0b6e086f09afcf6a94582a7c7529c4b2e9
Arohan24 <coool.aro24@gmail.com>
2024-03-02 22:37:22 +0530

    Frontend Started Header and Footer Done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Footer from './components/layout/Footer/Footer.js';
+import Header from  "./components/layout/Header/Header.js";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
